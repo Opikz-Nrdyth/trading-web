39da29ce8fc2655f4d0bc09b0ff1384df87d7e50
Opikzz <tofiqnurhidayat110@gmail.com>
2025-05-07 17:37:01 +0700

    clear optimize

--- storage/framework/views/090b16dce04f07068f71233930ddeab0.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title', __('Server Error')); ?>
-<?php $__env->startSection('code', '500'); ?>
-<?php $__env->startSection('message', __('Server Error')); ?>
-
-<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH H:\Bank Project\2025\trading web jadi\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/500.blade.php ENDPATH**/ ?>
